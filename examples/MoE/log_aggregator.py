2a91e089454ef855c970754287d7ea04dd82b526
Gabriel4256 <tla4256@gmail.com>
2022-09-02 04:12:24 +0000

    update log aggregator

--- examples/MoE/log_aggregator.py
+++ examples/MoE/log_aggregator.py
@@ -1,7 +1,9 @@
 import argparse
+from poplib import POP3_PORT
 import re
 from sys import prefix
 from turtle import forward
+from weakref import ref
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('file', type=str, action="store",
@@ -15,6 +17,17 @@ def parse_log(file_name, pp_level: int, mp_level: int):
     comm_overheads = []
     forward_passes = []
     forward_pass_per_rank = []
+    
+    all_reduce_latencies_per_inf = []
+    all_reduce_latencies_during_infer = []
+    all_reduce_latency_tmp = []
+    
+    
+    def finish_one_inference():
+        assert len(all_reduce_latency_tmp) == 0
+        if len(all_reduce_latencies_during_infer) > 0:
+            comm_overheads.append(sum(all_reduce_latencies_during_infer))
+            all_reduce_latencies_during_infer.clear()
 
     for line in f:
         # print(x)
@@ -71,6 +84,16 @@ def parse_log(file_name, pp_level: int, mp_level: int):
                         forward_pass_per_rank.clear()
                 else:
                     forward_passes.append(latency)
+                finish_one_inference()
+            elif title =="all_reduce latency":
+                latency = float(splitted[1].strip())
+                all_reduce_latency_tmp.append(latency)
+                if len(all_reduce_latency_tmp) == mp_level:
+                    longest_latency = max(all_reduce_latency_tmp)
+                    all_reduce_latencies_during_infer.append(longest_latency)
+                    all_reduce_latency_tmp.clear()
+            else:
+                pass
     # remove warmup
     
     print("file name: ", file_name)
@@ -82,12 +105,15 @@ def parse_log(file_name, pp_level: int, mp_level: int):
 
     if len(comm_overheads) > 0:
         comm_overheads.pop(0)
-        avg_comm_overhead_in_ms = sum(comm_overheads) / len(comm_overheads) * 1000
+        avg_comm_overhead_in_ms = sum(comm_overheads) / len(comm_overheads)
         print("\tavg comm overhead: {:.3f}ms".format(avg_comm_overhead_in_ms))
     else:
         avg_comm_overhead_in_ms = 0
 
     print("\tavg total latency: {:.3f}ms".format(avg_comm_overhead_in_ms + avg_fwd_pass_in_ms))
+
+
+
 
     # print("""file name: {},
     #     avg_fwd_pass: {:.3f}ms,
@@ -104,6 +130,16 @@ def parse_log(file_name, pp_level: int, mp_level: int):
     #           (avg_fwd_pass_in_ms + avg_comm_overhead_in_ms) * 100)
     #       )
 
+import re
+
+def find_par_level(target: str, file_name:str) -> int:
+    par_level = re.findall(target + r"\d+", file_name)
+    assert len(par_level) <= 1
+    if len(par_level) == 1:
+        par_level = int(par_level[0][2:])
+    else:
+        par_level = 1
+    return par_level
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -111,18 +147,26 @@ if __name__ == "__main__":
     prefix = 'pp2-g2-b'
 
     prefix = "dense-g1-b"
-    prefix = "mp2-g2-b" 
-    if prefix.split("-")[0].startswith("pp"):
-        pp_level = int(prefix.split("-")[0][-1])
-    else:
-        pp_level = 1
+    prefix = "mp2-g2-b"
+    prefix = "./result2/mp1-g1-b" 
+    # if prefix.split("-")[0].startswith("pp"):
+    #     pp_level = int(prefix.split("-")[0][-1])
+    # else:
+    #     pp_level = 1
     
-    if prefix.split("-")[0].startswith("mp"):
-        mp_level = int(prefix.split("-")[0][-1])
-    else:
-        mp_level = 1
+    # if prefix.split("-")[0].startswith("mp"):
+    #     mp_level = int(prefix.split("-")[0][-1])
+    # else:
+    #     mp_level = 1
 
-    file_names = [f"./{prefix}{i}.txt" for i in [1,2,4,8,16]]
+    # file_names = [f"{prefix}{i}.txt" for i in [1,2,4,8,16]]
+    # file_names = [f"result/mp{mp}-g{mp}-b{b}.txt" for b in [1,2,4,8,16] for mp in [1,2,3,4,6]]
+    file_names = [f"result2/mp{mp}-g{mp}-b{b}.txt" for b in [1,2,4,8,16] for mp in [1,2,4,8]]
+    
+    
     for file_name in file_names:
-        parse_log(file_name, pp_level=pp_level, mp_level=mp_level)
+        pp_level =find_par_level("pp", file_name)
+        mp_level =find_par_level("mp", file_name)
     # argparse.
+        
+        parse_log(file_name, pp_level=pp_level, mp_level=mp_level)
